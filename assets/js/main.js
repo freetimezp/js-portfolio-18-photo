e7c17c7f946c964c20dacb32527202e9e289f3af
freetimezp <kraftukrnet@ukr.net>
2023-06-10 20:39:48 +0300

    03. js-portfolio-18-photo. Add content.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,13 @@ window.onscroll = function () {
 
     frames.forEach(function (n, i) {
         zVals.push((i * zSpacing) + zSpacing);
-        zVals[i] += delta * -5;
+        zVals[i] += delta * -5.5;
 
         let frame = frames[i];
         let transform = `translateZ(${zVals[i]}px)`;
+        let opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
 
-        frame.setAttribute('style', `transform: ${transform}`);
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`);
     });
 }
 
